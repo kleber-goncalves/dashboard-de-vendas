41a155399e51fc5397dd9df2a251d7796c21e877
Kleber Lucas.G.S. <lenovomicrosof1@gmail.com>
2026-07-11 14:56:51 -0300

    feat: logout flow

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,21 +1,44 @@
-import { CardComponent, Header, StyledButton } from '@/components'
 import { useContext } from 'react'
 import { AppThemeContext } from '@/contexts/appthemeContext'
+
+// COMPONENTS
+import { CardComponent, Header, StyledH2, StyledButton } from '@/components'
+import { Container, Grid } from '@mui/material'
+
+// SERVICES
+import { logout } from '@/services'
 
 function Profile() {
     const themeContext = useContext(AppThemeContext)
     return (
         <>
             <Header />
-            <CardComponent>
-                <StyledButton
-                    className="primary"
-                    onClick={themeContext?.toggleTheme}
-                >
-                    Trocar para tema{' '}
-                    {themeContext?.appTheme === 'light' ? 'escuro' : 'claro'}
-                </StyledButton>
-            </CardComponent>
+            <Container className="mb-2" maxWidth="lg">
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6}>
+                        <CardComponent>Seus dados....</CardComponent>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <CardComponent>
+                            <StyledH2 className="mb-1">
+                                Definiçoes de conta
+                            </StyledH2>
+                            <StyledButton
+                                className="primary mb-1"
+                                onClick={themeContext?.toggleTheme}
+                            >
+                                Trocar para tema{' '}
+                                {themeContext?.appTheme === 'light'
+                                    ? 'escuro'
+                                    : 'claro'}
+                            </StyledButton>
+                            <StyledButton className="alert" onClick={logout}>
+                                Logout
+                            </StyledButton>
+                        </CardComponent>
+                    </Grid>
+                </Grid>
+            </Container>
         </>
     )
 }
